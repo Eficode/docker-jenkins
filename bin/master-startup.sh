c3797ccd845e212a8534e41796ec6d64262f6f27
Jouni Rajala <jouni.rajala@eficode.com>
2015-01-26 13:33:56 +0200

    Initial version of jenkins master

---
+++ bin/master-startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MASTER_NAME=jenkins-master
+CONTAINER_VERSION=snapshot
+HTTP_PORT=8080
+SLAVE_PORT=50000
+HOME_VOLUME=~/docker_jenkins_home/$NAME
+
+docker run --rm --name $MASTER_NAME -p $HTTP_PORT:8080 -p $SLAVE_PORT:50000 -v $HOME_VOLUME:/var/jenkins_home eficode/jenkins:$CONTAINER_VERSION
